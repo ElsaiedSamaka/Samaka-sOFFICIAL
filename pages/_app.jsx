0f50125b4b2641f33240e775a9bfe79cfe0cc1dc
unknown <elsaied.samaka1997@gmail.com>
2021-12-11 11:36:45 +0200

    setup themeing && dark mode

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,19 @@
-import '../styles/app.scss';
-
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import "../styles/app.scss";
+import { darkTheme, lightTheme, GlobalStyles } from "../ThemeConfig";
 // This default export is required in a new `pages/_app.js` file.
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    theme == "light" ? setTheme("dark") : setTheme("light");
+  };
+  return (
+    <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <button onClick={toggleTheme}>Switch Theme</button>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
