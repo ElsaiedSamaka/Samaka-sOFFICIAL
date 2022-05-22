2e18264f01c5e1eb6e761c0ccbbbd0563bf6d1af
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-05-22 16:28:27 +0200

    setup stylesheetManger + setup navbar trans...

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,12 +5,29 @@ import LangSwitchBtn from "./LangSwitchBtn";
 
 function Navbar({ router }) {
   const { theme, toggleTheme } = useTheme();
+  const { locale } = router;
 
   const navs = [
-    { text: "Home", href: "/" },
-    { text: "About", href: "/about" },
-    { text: "Portfolio", href: "/portfolio" },
-    { text: "Contact", href: "/contact" },
+    {
+      en: "Home",
+      ar: "الرئيسية",
+      href: "/",
+    },
+    {
+      en: "About",
+      ar: "عن",
+      href: "/about",
+    },
+    {
+      en: "Portfolio",
+      ar: "بروتوفوليو",
+      href: "/portfolio",
+    },
+    {
+      en: "Contact",
+      ar: "تواصل",
+      href: "/contact",
+    },
     // { text: '', href: '' },
   ];
 
@@ -23,15 +40,15 @@ function Navbar({ router }) {
         <LangSwitchBtn />
 
         <ul className='nav-links'>
-          {navs.map((nav) => (
-            <li key={nav.text}>
+          {navs.map((nav, i) => (
+            <li key={i}>
               <Link href={nav.href}>
                 <a
                   className={`nav-item ${
                     router.pathname == nav.href ? "active" : ""
                   }`}
                 >
-                  {nav.text}
+                  {nav[locale]}
                 </a>
               </Link>
             </li>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,28 @@
-import { ThemeProvider } from "styled-components";
+import { useRouter } from "next/router";
+import { StyleSheetManager, ThemeProvider } from "styled-components";
+import stylisRTLPlugin from "stylis-plugin-rtl";
 import { useTheme } from "../hooks/useTheme";
 import "../styles/app.scss";
 import { ThemeCTXProvider } from "../theme/ThemeProvider";
 import { GlobalStyles, lightTheme } from "../ThemeConfig";
-
+import getDirection from "../utils/translations/getDirections";
 // This default export is required in a new `pages/_app.js` file.
 
 export default function MyApp({ Component, pageProps }) {
   const { theme } = useTheme();
+  const { locale } = useRouter();
 
   return (
     <ThemeCTXProvider>
       {/* theme value seem to be changing propaply for some reason it's not updated on theme provider */}
-      <ThemeProvider theme={theme == "light" ? lightTheme : lightTheme}>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <StyleSheetManager
+        stylisPlugins={getDirection(locale) === "rtl" ? [stylisRTLPlugin] : []}
+      >
+        <ThemeProvider theme={theme == "light" ? lightTheme : lightTheme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StyleSheetManager>
     </ThemeCTXProvider>
   );
 }
