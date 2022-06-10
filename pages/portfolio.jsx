8e6b152145d4c5b105f2af1e6b14c4bdb7af05cd
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-06-10 15:16:26 +0200

    refactor

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -17,22 +17,32 @@ function Portfolio() {
             <div className='text-wrapper w-full'>
               <h1 className='title'>Portfolio</h1>
               <p className='description'>
-                Incididunt in cillum magna occaecat nisi qui in.
+                SOME OF MY WORK.
               </p>
 
               <div className='portfolio-wrapper'>
                 <div className='portfolio-item'>
-                  <img src='/chitchat.png' className='portfolio-image' />
-
-                  <h4 className='portfolio-name'>Chat App Exploration</h4>
-                  <div className='portfolio-category'>Mobile Dev</div>
+                  <img src='/SamakaStore.png' className='portfolio-image' />
+<a href="https://samaka-st-ore.vercel.app/" target="_blank">
+                    <h4 className='portfolio-name'>SamakaSTore</h4>
+                    </a>
+                  <div className='portfolio-category'>a fully feldged nextjs ecommerce wep app</div>
                 </div>
                 <div className='portfolio-item'>
-                  <img src='/kajian.jpg' className='portfolio-image' />
+                  <img src='/notesapp.png' className='portfolio-image' />
+<a href="https://react-tailwind-notes.netlify.app/" target="_blank">
 
-                  <h4 className='portfolio-name'>kajian App</h4>
-                  <div className='portfolio-category'>Mobile Dev</div>
+                  <h4 className='portfolio-name'>Notes App</h4></a>
+                  <div className='portfolio-category'>CRUD project ( React + tailwind + browser localstorage )</div>
                 </div>
+                 <div className='portfolio-item'>
+                  <img src='/vpnland.png' className='portfolio-image' />
+<a href="https://next-vpn-land.netlify.app/" target="_blank">
+
+                  <h4 className='portfolio-name'>VPN Landingpage</h4></a>
+                  <div className='portfolio-category'>Next VPN open source landing page template</div>
+                </div>
+                
               </div>
             </div>
           </div>
@@ -41,5 +51,5 @@ function Portfolio() {
     </>
   );
 }
-
+// 
 export default Portfolio;
